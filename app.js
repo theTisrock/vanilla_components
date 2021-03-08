867e20271e647058c069bbb93e873474a62a8e4f
chris torok <torok.chris@gmail.com>
2021-03-07 21:58:01 -0500

    refactor: encapsulate component into a class

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /* Table data - data that drives the table construction */
 
 // mock data
-let columnTitles = ['number', 'first name', 'last name', 'year'];
-let tableData = [
+const columnHeaders = ['number', 'first name', 'last name', 'year'];
+const tableData = [
     [1, 'george', 'washington', '1789-1796'],
     [2, 'john', 'adams', '1797-1801'],
     [3, 'thomas', 'jefferson', '1801-1809'],
@@ -10,63 +10,79 @@ let tableData = [
     [5, 'james', 'monroe', '1817-1825'],
 ];
 
-function fillTableHeaders(titles, thead) {
-    for (let i=0; i<titles.length; i++) {
-        let th = document.createElement('th');
-        th.textContent = titles[i];
-        thead.appendChild(th);
-    }
-}
-
-function fillTableBody(array2d, tbody, rowDataLimit) {
-    for (let row=0; row<array2d.length; row++) {
-        let tr = document.createElement('tr');
-
-        let limiter = rowDataLimit < array2d[row].length ? rowDataLimit : array2d[row].length;
-
-        for (let col=0; col<limiter; col++) {
-            let td = document.createElement('td');
-            td.textContent = array2d[row][col];
-            tr.appendChild(td);
-        }
-        tbody.appendChild(tr);
-    }
-}
-
-function buildTableComponents(titles, tableData2dArray, name) {
-    const table = document.createElement('table');
-    table.setAttribute('id', `${name}`);
-    table.classList.add('u-full-width');
-    
-    const thead = document.createElement('thead');
-    // fill thead section with data from titles array
-    fillTableHeaders(titles, thead);
-    // create tbody section
-    const tbody = document.createElement('tbody');
-    // fill tbody with data from tableData2dArray
-    fillTableBody(tableData2dArray, tbody, titles.length);
-    // attach the thead as child to table
-    table.appendChild(thead);
-    // attach the tbody as child to table
-    table.appendChild(tbody);
-    
-    return table;
-}
-
-function displayTable(titles, data, tableName) {
-    const table = buildTableComponents(titles, data, tableName);
-    const label = document.createElement('label');
-    label.setAttribute('for', `${tableName}`);
-    label.textContent = tableName;
-    const tableArea = document.querySelector('#table-area');
-    tableArea.appendChild(label);
-    tableArea.appendChild(table);
-}
-
-
-displayTable(columnTitles, tableData, 'US Presidents');
-
 
 class DynamicTable {
     
+    constructor(columnHeaders, tableData) {
+        this.columnHeaders = columnHeaders;
+        this.tableData = tableData;
+        this.tableName = null;
+        this.tableId = null;
+    }
+
+    setTableName(name) {
+        this.tableName = name;
+    }
+
+    setTableId(id) {
+        this.tableId = id;
+    }
+
+    fillTableHeaders(columnHeaders, thead) {
+        for (let i=0; i<columnHeaders.length; i++) {
+            let th = document.createElement('th');
+            th.textContent = columnHeaders[i];
+            thead.appendChild(th);
+        }
+    }
+
+    fillTableBody(tableData, tbody, rowDataLimit) {
+        for (let row=0; row<tableData.length; row++) {
+            let tr = document.createElement('tr');
+            let limiter = rowDataLimit < tableData[row].length ? rowDataLimit : tableData[row].length;
+    
+            for (let col=0; col<limiter; col++) {
+                let td = document.createElement('td');
+                td.textContent = tableData[row][col];
+                tr.appendChild(td);
+            }
+            tbody.appendChild(tr);
+        }
+    }
+
+    buildTableComponents() {
+        const table = document.createElement('table');
+
+        const thead = document.createElement('thead');
+        this.fillTableHeaders(this.columnHeaders, thead);
+        
+        const tbody = document.createElement('tbody');
+        this.fillTableBody(this.tableData, tbody, this.columnHeaders.length);
+        
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        
+        return table;
+    }
+
+    display(attachTo) {
+        // grab table
+        const table = this.buildTableComponents();
+
+        // prepare table
+        table.setAttribute('id', this.tableId);
+        const label = document.createElement('label');
+        label.textContent = this.tableName;
+        const tableArea = document.querySelector(attachTo);
+    
+        // display
+        tableArea.appendChild(label);
+        tableArea.appendChild(table);
+    }
 }
+
+const USPresidents = new DynamicTable(columnHeaders, tableData);
+USPresidents.buildTableComponents();
+USPresidents.setTableName('US Presidents');
+USPresidents.setTableId('#us-presidents');
+USPresidents.display('#table-area');
